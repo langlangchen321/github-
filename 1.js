9a1b962ee41b438b02f232f9c1e02d8f54c698fe
v_langchen(陈郎) <v_langchen@tencent.com>
2019-04-29 17:35:04 +0800

    修改了函数

--- 1.js
+++ 1.js
@@ -7,28 +7,7 @@
 	<body>
 	</body>
 	<script type="text/javascript">
-		{/* var name='window';
-		
-		var obj={
-			name:'obj',
-			getName:function () {
-				return function (){
-					return this.name
-				}
-			}
-		}		
-		console.log(obj.getName()());//window */}
-		var name='chenlang';
-		
-		var obj1={
-			name:'haha',
-			getName:function () {
-				return function (){
-					return this.name
-				}
-			}
-		}		
-		console.log(obj1.getName()());//window
+	
 		
 	</script>
 </html>
